a91d98ca48d78314f7c5976f8d40e3c24a24491b
Andreas Rosland <andreas.rosland@copyleft.no>
2018-06-15 14:46:52 -0500

    webwash function

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -6,9 +6,18 @@ alias puppetdeploy='ssh root@puppet.cl.no r10k deploy environment production -v 
 alias vc='vimcat'
 alias vp='vimpager'
 alias vim="nvim"
+alias mplayer="mpv"
 
+#convert cr2 files to jpg
+function cr2jpg { for i in *.CR2; do sips -s format jpeg $i --out "${i%.*}.jpg"; done; }
 #shared host reverse lookup
-#function shdig {dig "$1" +short | xargs dig +short -x; }
+function shdig { dig "$1" +short | xargs dig +short -x; }
+
+#short dig
+function sdig { dig "$1" +short; }
+
+#webwash lookup
+function webwash {  ssh webwash01.clh.no "cat /etc/apache2/sites-enabled/$1*" | grep -v "#"; }
 
 #TLS cert check
 tlscert () {
@@ -16,7 +25,7 @@ tlscert () {
 }
 
 # markdown commandline reader
-#function mdread {pandoc -t plain $1 | less; }
+#function mdread { pandoc -t plain $1 | less; }
 
 # mosh tmux attach, should be improved so I can map it to mosh
-#function mosht {mosh $1 -- tmux attach -t 0 -d; }
+function mosht { mosh $1 -- tmux attach -t 0 -d; }
